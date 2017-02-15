2992cc1b4e046d628228584e64fd3acd0663656e
p-Mart <phillip.martin012@gmail.com>
2017-02-15 17:41:41 -0500

    Added Conway's life rule, ability to draw and erase cells on grid, along with pause and reset features for the environment. Need to make grid more efficient.

--- sketch.js
+++ sketch.js
@@ -1,20 +1,36 @@
-var cell_width = 8;
-var cell_height= 8;
+//Globals / constants
+var cell_width = 12;
+var cell_height= 12;
 
 var cells;
+
+//Control Variables
+var paused = false;
+var reset = false;
+var drawing = false;
+var draw_mode = false;
+var grid_on = false;
 
 function Cell(i, j, is_on){
 	this.x_position = i*cell_width;
 	this.y_position = j*cell_height;
 	this.is_on = is_on;
-	this.adjacentcellstates = [false,false,false,false,false,false,false,false];	
+	this.adjacentcellstates = [false,false,false,false, false, false,false,false,false];
+	this.sumstates = 0;	
 }
 
 Cell.prototype.getNeighborStates = function(i, j){
-	for(k = 0; k < 8; k++){
-		var x_index = i + ((floor(k/3)) - 1);
-		var y_index = j + ((k%3) - 1);
+	for(k = 0; k < 9; k++){
+		var x_index = i + ((k%3) - 1);
+		var y_index = j + (floor(k/3)) - 1;
 		
+		//The state of the cell being operated on
+		if(k == 4){
+			this.adjacentcellstates[k] = this.is_on;
+			continue;
+		}
+		
+		//The states of the cells adjacent to the cell being operated on
 		if(x_index < 0 || x_index >= (width / cell_width) || y_index < 0 || y_index >= (height / cell_height))
 			this.adjacentcellstates[k] = false;
 		else{
@@ -22,11 +38,23 @@ Cell.prototype.getNeighborStates = function(i, j){
 			this.adjacentcellstates[k] = cell.is_on;
 		}
 	}
+
+	var count = 0;
+	for(i = 0; i < 9; i++){
+		if (i == 4){
+			continue;
+		}
+		if(this.adjacentcellstates[i] == true){
+			count = count + 1;
+		}
+	}
+	this.sumstates = count;
+
 	return;
 }
 
-Cell.prototype.update = function(rule_number){
-	this.is_on = rule(rule_number, this.adjacentcellstates);
+Cell.prototype.update = function(){
+	this.is_on = rule(this.adjacentcellstates, this.sumstates);
 }
 
 function initializeCells(rows,cols){
@@ -42,17 +70,20 @@ function initializeCells(rows,cols){
 	return arr;
 }
 
-function rule(rule_number, adjacentcellstates){
-	var count = 0;
-	for (x in adjacentcellstates){
-		if(x == true){
-			count = count + 1;
-		}
+function rule(adjacentcellstates, sumstates){
+	var count = sumstates;
+	if (adjacentcellstates[4] == true && count < 2){
+		return false;
 	}
-	if (count == 1){
+	else if (adjacentcellstates[4] == true && (count == 2 || count == 3)){
 		return true;
 	}
-	else{
+	else if (adjacentcellstates[4] == true && count > 3){
+		return false;
+	}
+	else if (adjacentcellstates[4] == false && count == 3){
+		return true;
+	}else{
 		return false;
 	}
 }
@@ -61,38 +92,111 @@ function setup() {
 	createCanvas(windowWidth,windowHeight);
 	cells = initializeCells(width / cell_width, height / cell_height);
 	cells[50][50].is_on = true;
-}
-
-function draw() {
-	//strokeWeight(random(1,6));
-	//point(random(0,width), random(0, height));
-	background(12,12,12);
-	noStroke();
+	cells[51][50].is_on = true;
+	cells[49][50].is_on = true;
+	cells[51][49].is_on = true;
+	cells[50][48].is_on = true;
+	textSize(10);
+	textAlign(CENTER);
+	frameRate(15);
 	/*
-	for(i = 0; i < windowWidth / cell_width;i++){
-		for(j = 0; j < windowHeight / cell_height; j++){
-			fill(random(0,255),0,random(0,255));
-			if(cells[i][j].is_on == false){
-				rect(cells[i][j].x_position,cells[i][j].y_position, cell_width,cell_height);
-			}
-		}
-	}
-	*/
-	fill(255,255,255)
-	for(i = 0; i < windowWidth / cell_width;i++){
-		for(j = 0; j < windowHeight / cell_height;j++){
+	for(i = 0; i < width / cell_width;i++){
+		for(j = 0; j < height / cell_height;j++){
 			cells[i][j].getNeighborStates(i,j);
 		}
 	}
-	for(i = 0; i < windowWidth / cell_width;i++){
-		for(j = 0; j < windowHeight / cell_height;j++){
-			cells[i][j].update(12);
-			if(cells[i][j].is_on == true){
-				rect(cells[i][j].x_position,cells[i][j].y_position, cell_width,cell_height);
+	
+
+	for(i = 0; i < width / cell_width;i++){
+		for(j = 0; j < height / cell_height;j++){
+			cells[i][j].update();
+		}
+	}
+	*/
+
+}
+
+function draw() {
+
+	background(12,12,12);
+	noStroke();
+	fill(255,255,255);
+
+	//Draw Modes
+	if (drawing == true && draw_mode == false){
+		cells[floor(mouseX/cell_width)][floor(mouseY/cell_height)].is_on = true;
+	}
+	else if (drawing == true && draw_mode == true){
+		cells[floor(mouseX/cell_width)][floor(mouseY/cell_height)].is_on = false;
+	}
+
+	//Reset the environment.
+	if(reset == true){
+		background(12,12,12);
+		cells = initializeCells(width / cell_width, height / cell_height);
+		reset = false;
+	}
+
+	//Update the neighboring states of each cell if environment is unpaused.
+	if(paused == false){
+		for(i = 0; i < width / cell_width;i++){
+			for(j = 0; j < height / cell_height;j++){
+				cells[i][j].getNeighborStates(i,j);
 			}
 		}
 	}
-	
-	//rect(cells[50][50].x_position,cells[50][50].y_position, cell_width,cell_height);
 
+	//Update and Draw. Drawing happens regardless of whether environment is paused.
+	for(i = 0; i < width / cell_width;i++){
+		for(j = 0; j < height / cell_height;j++){
+
+			if(paused == false){
+				cells[i][j].update();
+			}
+			if(cells[i][j].is_on == false && grid_on == true){
+				stroke(255);
+				noFill();
+				rect(cells[i][j].x_position,cells[i][j].y_position, cell_width,cell_height);
+			}
+			if(cells[i][j].is_on == true){
+				noStroke();
+				fill(255,255,255);
+				rect(cells[i][j].x_position,cells[i][j].y_position, cell_width,cell_height);
+			} 
+			//fill(255,0,0);
+			//text(str(cells[i][j].sumstates),cells[i][j].x_position,cells[i][j].y_position,cell_width,cell_height);
+		}
+	}
+} 
+
+
+function keyPressed(){
+	//Spacebar
+	if (keyCode == 32){
+		paused = !paused;
+	}
+	else if(key == "R"){
+		reset = true;
+	}
+	else if(key == "E"){
+		draw_mode = !draw_mode;
+	}
+	else if(key == "G"){
+		grid_on = !grid_on;
+	}
+	return false;
+}
+
+function mousePressed(){
+	if (mouseButton == LEFT){
+		drawing = true;
+	}
+	return false;
+}
+
+function mouseReleased(){
+	if (mouseButton == LEFT){
+		drawing = false;
+	}
+	return false;
 }
